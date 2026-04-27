96f98e88c91910b5b2c9e94df1e7bcbd296373f7
s7cret <s7cret@users.noreply.github.com>
2026-04-27 15:24:41 +0300

    Release pine2ast v3.7 oracle expansion pending

--- pine2ast/__init__.py
+++ pine2ast/__init__.py
@@ -1,7 +1,7 @@
 from .api import ParseOptions, ParseResult, ast_to_dict, ast_to_json, parse_code, parse_file
 from .ast.schema import SchemaReport, validate_ast_schema
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 
 __all__ = [
     "ParseOptions",
